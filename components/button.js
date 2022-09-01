68f5b02a9d40ded25f85d8950a5d6f8c731e0be1
aesweb <aes.sncr@gmail.com>
2022-09-02 01:48:44 +0300

    navigation component is created

--- components/button.js
+++ components/button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './button.module.css';
 
-function Button({ children }) {
+function Button({ children, ...props }) {
   return (
-    <button type="button" className={styles.button}>
+    <button type="button" className={styles.button} {...props}>
       {children}
     </button>
   );
