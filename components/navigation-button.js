fac9eead1c39cb28b2351feb89e9407f2167ca9d
aesweb <aes.sncr@gmail.com>
2022-09-03 20:18:12 +0300

    navigation component is done.

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 import Button from './button';
 import styles from './navigation-button.module.css';
 import '../styles/variables.css';
 
-function NavigationButton({ children }) {
-  return <Button className={styles.navButton}>{children}</Button>;
+function NavigationButton({ notify, selected, children, ...props }) {
+  return (
+    <Button
+      className={cn(styles.navButton, selected && styles.navButtonSelected)}
+      {...props}
+    >
+      {notify && <span className={styles.notify}>{notify}</span>}
+      {children}
+    </Button>
+  );
 }
 
 export default NavigationButton;
--- stories/1-Button.stories.jsx
+++ stories/1-Button.stories.jsx
@@ -3,11 +3,18 @@ import React from 'react';
 import Button from '../components/button';
 import NavigationButton from '../components/navigation-button';
 import Navigation from '../components/navigation';
+import { Home } from '../components/icons';
+import TitleBold from '../components/title-bold';
 
 export default {
   title: 'Buttons'
 };
 
 export const Normal = () => <Button>Save</Button>;
-export const NavButton = () => <NavigationButton>Save</NavigationButton>;
-export const Nav = () => <Navigation />;
+export const NavButton = () => (
+  <NavigationButton>
+    <Home />
+    <TitleBold>Home</TitleBold>
+  </NavigationButton>
+);
+export const Nav = () => <Navigation selectedKey="home" />;
