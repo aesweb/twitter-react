2d89bf86e7378205fe95d287296c4e1b9e4283b8
aesweb <aes.sncr@gmail.com>
2022-09-12 03:40:18 +0300

    clones created for layout

---
+++ components/col-sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import cn from 'classnames';
+import Navigation from './navigation';
+import ThemeButton from './theme-button';
+import ProfileBox from './profile-box';
+
+import styles from './col-sidebar.module.css';
+
+function Sidebar({ flat }) {
+  return (
+    <div className={cn([styles.sidebar])}>
+      <Navigation flat={flat} />
+      <ThemeButton big full>
+        Tweet
+      </ThemeButton>
+      <ProfileBox />
+    </div>
+  );
+}
+
+export default Sidebar;
