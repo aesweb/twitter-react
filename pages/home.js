64d96eea7ab4899662dfa791e2cb3be22391a7fd
aesweb <aes.sncr@gmail.com>
2022-09-16 19:39:53 +0300

    tweet example added on homepage

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
 import Layout from '../components/layout/index';
-import useWindowSize from '../hooks/useWindowSize';
+import Tweet from '../components/tweet';
 
 function HomePage() {
-  const size = useWindowSize();
   return (
     <Layout>
-      {JSON.stringify(size)} <br />
-      Home
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`TOKİ İlk Taksit         TOKİ Son Taksit`}
+        reply={'3'}
+        retweet={12}
+        like={215}
+      ></Tweet>
+
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`🔴#SONDAKİKA
+Türkiye, Rus doğalgazı için ödemelerinin %25'ini Ruble ile yapacak. 
+        
+Rus şirketleri, ürenlerini Türkiye üzerinden ihraç edebileceklerinin sinyalini aldı.`}
+        reply={'22'}
+        retweet={1}
+        like={33}
+      ></Tweet>
     </Layout>
   );
 }
