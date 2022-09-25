854e135cdd9313174c7f62fe8757dc3737b550ef
aesweb <aes.sncr@gmail.com>
2022-09-25 18:48:09 +0300

    (fix) and todo added

--- pages/home.js
+++ pages/home.js
@@ -28,6 +28,116 @@ Rus şirketleri, ürenlerini Türkiye üzerinden ihraç edebileceklerinin sinyal
         retweet={1}
         like={33}
       ></Tweet>
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`TOKİ İlk Taksit         TOKİ Son Taksit`}
+        reply={'3'}
+        retweet={12}
+        like={215}
+      ></Tweet>
+
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`🔴#SONDAKİKA
+Türkiye, Rus doğalgazı için ödemelerinin %25'ini Ruble ile yapacak. 
+        
+Rus şirketleri, ürenlerini Türkiye üzerinden ihraç edebileceklerinin sinyalini aldı.`}
+        reply={'22'}
+        retweet={1}
+        like={33}
+      ></Tweet>
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`TOKİ İlk Taksit         TOKİ Son Taksit`}
+        reply={'3'}
+        retweet={12}
+        like={215}
+      ></Tweet>
+
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`🔴#SONDAKİKA
+Türkiye, Rus doğalgazı için ödemelerinin %25'ini Ruble ile yapacak. 
+        
+Rus şirketleri, ürenlerini Türkiye üzerinden ihraç edebileceklerinin sinyalini aldı.`}
+        reply={'22'}
+        retweet={1}
+        like={33}
+      ></Tweet>
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`TOKİ İlk Taksit         TOKİ Son Taksit`}
+        reply={'3'}
+        retweet={12}
+        like={215}
+      ></Tweet>
+
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`🔴#SONDAKİKA
+Türkiye, Rus doğalgazı için ödemelerinin %25'ini Ruble ile yapacak. 
+        
+Rus şirketleri, ürenlerini Türkiye üzerinden ihraç edebileceklerinin sinyalini aldı.`}
+        reply={'22'}
+        retweet={1}
+        like={33}
+      ></Tweet>
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`TOKİ İlk Taksit         TOKİ Son Taksit`}
+        reply={'3'}
+        retweet={12}
+        like={215}
+      ></Tweet>
+
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`🔴#SONDAKİKA
+Türkiye, Rus doğalgazı için ödemelerinin %25'ini Ruble ile yapacak. 
+        
+Rus şirketleri, ürenlerini Türkiye üzerinden ihraç edebileceklerinin sinyalini aldı.`}
+        reply={'22'}
+        retweet={1}
+        like={33}
+      ></Tweet>
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`TOKİ İlk Taksit         TOKİ Son Taksit`}
+        reply={'3'}
+        retweet={12}
+        like={215}
+      ></Tweet>
+
+      <Tweet
+        name={'Ahmet Eren'}
+        slug={'aeswebb'}
+        dateTime={new Date('9.16.2022')}
+        text={`🔴#SONDAKİKA
+Türkiye, Rus doğalgazı için ödemelerinin %25'ini Ruble ile yapacak. 
+        
+Rus şirketleri, ürenlerini Türkiye üzerinden ihraç edebileceklerinin sinyalini aldı.`}
+        reply={'22'}
+        retweet={1}
+        like={33}
+      ></Tweet>
     </Layout>
   );
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+[] pages/ home , index will be updated
+[] loading... will be added
+[] twitter api wil be added (twitter developer account)
+[] submit tweet feature will be added
